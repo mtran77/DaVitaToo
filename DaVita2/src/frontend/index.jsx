4e2549c6ae6ff4b1a4a09324ecefbec891b3bf6c
EHWesson <shwesson@crimson.ua.edu>
2025-03-23 22:26:31 -0500

    change to manifest.yml for hosting

--- DaVita2/src/frontend/index.jsx
+++ DaVita2/src/frontend/index.jsx
@@ -1,9 +1,9 @@
-// import React from 'react';
-// import ForgeReconciler from '@forge/react';
-// import App from './app.jsx';
+import React from 'react';
+import ForgeReconciler from '@forge/react';
+import App from './app.jsx';
 
-// ForgeReconciler.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ForgeReconciler.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
