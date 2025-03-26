e2b0abf0b3b145e320598113c46372a85bc49e01
mtran77 <113166372+MT263@users.noreply.github.com>
2025-03-26 17:16:04 -0500

    Recent

--- build/src/frontend/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ForgeReconciler from '@forge/react';
-import App from './app.jsx';
-
-ForgeReconciler.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
